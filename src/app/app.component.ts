dd4201483e11ccd99aa2b08121e55ba22242e816
Sampath Bandla <sampath@pixelvide.com>
2024-02-26 00:52:05 +0530

    installed tailwind flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {initFlowbite} from "flowbite";
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'billwise-angular';
+export class AppComponent implements OnInit {
+  title = 'Billwise';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
